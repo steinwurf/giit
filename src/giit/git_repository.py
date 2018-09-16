7a2c40b17c304614bbe44317c1e21dbf5d43d18e
Morten V. Pedersen <morten@mortenvp.com>
2018-09-16 23:11:24 +0200

    working on it

--- src/giit/git_repository.py
+++ src/giit/git_repository.py
@@ -6,7 +6,6 @@ import json
 class GitRepository(object):
 
     def __init__(self, repository, git, git_url_parser, clone_path, log):
-                 # source_branch, log):
         """ Create a new instance.
 
         :param repository: The repository either as an URL or path to a
@@ -24,7 +23,6 @@ class GitRepository(object):
         self.clone_path = os.path.abspath(os.path.expanduser(clone_path))
         self.log = log
 
-    @property
     def workingtree_path(self):
         """ :return: The path to the workingtree if there is not workingtree
                      return None
@@ -34,7 +32,6 @@ class GitRepository(object):
         else:
             return None
 
-    @property
     def git_url(self):
         """ :return: The Git repository URL """
         if os.path.isdir(self.repository):
@@ -42,53 +39,65 @@ class GitRepository(object):
         else:
             return self.repository
 
-    @property
     def unique_name(self):
         """ :return: A unique name for this repository """
         # Compute the clone path
-        git_info = self.git_url_parser.parse(url=self.git_url)
+        git_url = self.git_url()
+        git_info = self.git_url_parser.parse(url=git_url)
 
-        url_hash = hashlib.sha1(self.git_url.encode('utf-8')).hexdigest()[:6]
+        url_hash = hashlib.sha1(git_url.encode('utf-8')).hexdigest()[:6]
         return git_info.name + '-' + url_hash
 
-    @property
-    def giit_clone_path(self):
+    def repository_path(self):
         """ :return: The path where we clone the repository """
-        return os.path.join(self.clone_path, self.unique_name)
+        return os.path.join(self.clone_path, self.unique_name())
 
-    @property
     def source_branch(self):
-        """ :return: The source branch. When passing a path to giit the
-                     source branch will be the branch currently checked out
-                     for that path. When passing an URL to giit we return None
+        """ The source branch.
+
+        When passing a path to giit the source branch will be the branch
+        currently checked out for that path. When passing an URL to giit we
+        return None
         """
 
-        if not self.workingtree_path:
+        # If we were not passed an URL
+        if not self.workingtree_path():
             return None
 
+        # Check if the current branch has a corresponding remote i.e. if
+        # it has been pushed.
         current = self.git.current_branch(cwd=self.workingtree_path)
 
-        remotes = self.git.remote_branches(cwd=self.giit_clone_path)
+        # Fetch a list of remote branches
+        remote_branches = self.git.remote_branches(cwd=self.repository_path)
 
-        match = [remote for remote in remotes if current in remote]
+        # The remote branches are in a list of "remote/branch"
+        match = []
+        for remote_branch in remote_branches:
+            remote, branch = remote_branch.split("/")
+
+            if branch == current:
+                match.append(remote_branch)
+
         matches = len(match)
 
         if matches == 0:
-            self.log.info(
-                "Skipping build: No remote branch tracking %s. These remote "
-                "branches were found in "
+            self.log.debug(
+                "Skipping source branch: No remote branch tracking %s. These "
+                "remote branches were found in "
                 "the repository: %s.\nYou probably just need to "
                 "push the branch you are working on:\n\n"
-                "\tgit push -u origin %s\n" % (current, remotes, current))
+                "\tgit push -u origin %s\n" % (current, remote_branches,
+                                               current))
 
             return None
 
         if matches > 1:
             raise RuntimeError(
-                "Several remote branches %s for %s" % (remotes, current))
+                "Several remote branches %s for %s" % (remote_branches, current))
 
         remote = match[0]
-        self.log.debug('branch %s -> %s', current, remote)
+        self.log.debug('source branch %s' % remote)
         return remote
 
     def clone(self):
@@ -102,101 +111,90 @@ class GitRepository(object):
                       ".".join([str(i) for i in self.git.version()]))
 
         # Get the URL to the repository
-        self.log.info("Using git repository: %s", self.git_url)
+        git_url = self.git_url()
+        self.log.info("Using git repository: %s", git_url)
+
+        repository_path = self.repository_path()
 
         # Get the updates
-        if os.path.isdir(self.giit_clone_path):
-            self.log.info('Running: git fetch in %s', self.giit_clone_path)
-            self.git.fetch(cwd=self.giit_clone_path, all=True, prune=True)
+        if os.path.isdir(repository_path):
+            self.log.info('Running: git fetch in %s', repository_path)
+            self.git.fetch(cwd=repository_path, all=True, prune=True)
+
         else:
-            self.log.info('Running: git clone into %s', self.giit_clone_path)
-            self.git.clone(repository=self.git_url,
-                           directory=self.giit_clone_path,
-                           cwd=self.clone_path)
+            self.log.info('Running: git clone into %s', repository_path)
+            self.git.clone(repository=git_url, directory=repository_path,
+                           cwd=self.clone_path
 
-    def checkout(self, checkout):
-        """ Ensures that the repository is on a specific checkout.
+    def remote_branches(self):
+        """ :return: The re,pte brances specified for the repository """
+        assert os.path.isdir(self.repository_path())
 
-        The checkout can be a branch name, commit or tag.
-        """
-
-        # Check if this is a branch
-        remotes = self.git.remote_branches(cwd=self.giit_clone_path)
-
-        match = [remote for remote in remotes if current in remote]
-        matches = len(match)
-
-        if matches == 0:
-            self.log.debub(
-                "No remote branch tracking %s. These remote "
-                "branches were found in "
-                "the repository: %s." % (current, remotes))
-
-        if matches > 1:
-            raise RuntimeError(
-                "Several remote branches %s for %s" % (remotes, current))
-
-        remote = match[0]
-        self.log.debug('branch %s -> %s', current, remote)
-        return remote
-
-    def checkout_branch(self, checkout):
-        """ Checkout a specific branch.
-
-        The branch must be a remote-tracking branch, but passing both
-        master or origin/master should work.
-        """
-
-        # Check if this is a branch
-        remotes = self.git.remote_branches(cwd=self.giit_clone_path)
-
-        match = [remote for remote in remotes if checkout in remote]
-        matches = len(match)
-
-        if matches == 0:
-            self.log.debug(
-                "No remote branch tracking %s. These remote "
-                "branches were found in "
-                "the repository: %s." % (checkout, remotes))
-
-        if matches > 1:
-            raise RuntimeError(
-                "Several remote branches %s for %s" % (remotes, checkout))
-
-        remote = match[0]
-        self.log.debug('branch %s -> %s', checkout, remote)
-        return remote
+        return self.git.remote_branches(cwd=self.repository_path())
 
     def tags(self):
         """ :return: The tags specified for the repository """
-        return self.git.tags(cwd=self.giit_clone_path)
+        assert os.path.isdir(self.repository_path())
 
-    def load_json_config(self):
+        return self.git.tags(cwd=self.repository_path())
 
-        if self.workingtree_path:
-            json_config = os.path.join(self.workingtree_path, 'giit.json')
+    def checkout_branch(self, remote_branch):
+        """ Checkout a specific branch.
 
-            self.log.info("Using giit.json from %s workingtree",
-                          self.workingtree_path)
+        The branch must be a remote-tracking branch.
+        """
 
-            with open(json_config, 'r') as config_file:
-                return json.load(config_file)
+        # Check if the branch exists
+        remotes=self.remote_branches()
 
-        # We only support building branches remote branches. The reason for
-        # this it that it makes it easier to know what is the state of a given
-        # branch. Two users might be on the same branch but have different
-        # changes. Since we only build the remote we force users to push their
-        # changes and thereby we have one source of truth of what is on a given
-        # branch. Namely what has been pushed to the remote. Lets get the
-        # corresponding remote branch
+        if remote_branch not in remotes:
+            raise RuntimeError("No remote branch %s. These branches exits "
+                               "in the repository %s" % (remote_branch, remotes))
 
-        # Make sure we start on the source branch, we may
-        # read the giit.json file from here
-        self.git.checkout(branch=self.giit_branch, cwd=self.giit_clone_path)
+        self._checkout(checkout=remote_branch)
 
-        json_config = os.path.join(self.giit_clone_path, 'giit.json')
+    def checkout_tag(self, tag):
+        """ Checkout a specific tag."""
 
-        self.log.info("Using giit.json from %s branch", self.giit_branch)
+        # Check if the tag exists
+        tags=self.tags()
 
-        with open(json_config, 'r') as config_file:
-            return json.load(config_file)
+        if tag not in tags:
+            raise RuntimeError("No tag %s. These tags exits "
+                               "in the repository %s" % (tag, tags))
+
+        self._checkout(checkout=tag)
+
+    def _checkout(self, checkout):
+        # https://stackoverflow.com/a/8888015/1717320
+        self.git.reset(branch=checkout, hard=True, cwd=self.giit_clone_path)
+
+    # def load_json_config(self):
+
+    #     if self.workingtree_path:
+    #         json_config = os.path.join(self.workingtree_path, 'giit.json')
+
+    #         self.log.info("Using giit.json from %s workingtree",
+    #                       self.workingtree_path)
+
+    #         with open(json_config, 'r') as config_file:
+    #             return json.load(config_file)
+
+    #     # We only support building branches remote branches. The reason for
+    #     # this it that it makes it easier to know what is the state of a given
+    #     # branch. Two users might be on the same branch but have different
+    #     # changes. Since we only build the remote we force users to push their
+    #     # changes and thereby we have one source of truth of what is on a given
+    #     # branch. Namely what has been pushed to the remote. Lets get the
+    #     # corresponding remote branch
+
+    #     # Make sure we start on the source branch, we may
+    #     # read the giit.json file from here
+    #     self.git.checkout(branch=self.giit_branch, cwd=self.giit_clone_path)
+
+    #     json_config = os.path.join(self.giit_clone_path, 'giit.json')
+
+    #     self.log.info("Using giit.json from %s branch", self.giit_branch)
+
+    #     with open(json_config, 'r') as config_file:
+    #         return json.load(config_file)
--- test/test_git_repository.py
+++ test/test_git_repository.py
@@ -1,54 +1,149 @@
+import os
 import mock
 import logging
 
 import giit.factory
 
 
-def _test_git_repository(testdirectory, caplog):
-
-    caplog.set_level(logging.INFO)
-
-    giit_dir = testdirectory.mkdir('giit')
-    repo_dir = testdirectory.mkdir('repo')
-    repo_dir.run('git init')
-
-    factory = giit.factory.resolve_factory(
-        giit_path=giit_dir.path(), repository=repo_dir.path())
+def fake_git():
+    """ Create an return a fake git object we can use for testing. """
 
     git = mock.Mock()
     git.version.return_value = (1, 2, 3)
     git.remote_origin_url.return_value = "git@github.com:steinwurf/giit.git"
 
+    git.remote_branches.return_value = ["origin/add-prune",
+                                        "origin/improvements-v2",
+                                        "origin/initial-push",
+                                        "origin/master", ]
+
+    git.tags.return_value = ["1.0.0", "1.0.1", "1.0.2", "1.0.3",
+                             "2.0.0", "2.1.0", "3.0.0"]
+
+    return git
+
+
+# def test_git_repository_clone(testdirectory, caplog):
+
+#     caplog.set_level(logging.INFO)
+
+#     giit_dir = testdirectory.mkdir('giit')
+#     repo_dir = testdirectory.mkdir('repo')
+
+#     factory = giit.factory.resolve_factory(
+#         giit_path=giit_dir.path(), repository=repo_dir.path())
+
+#     git = fake_git()
+
+#     factory.provide_value(name='git', value=git, override=True)
+
+#     git_repository = factory.build()
+
+#     # The git repository has not yet been created so we should see a clone
+#     assert not os.path.isdir(git_repository.giit_clone_path)
+
+#     git_repository.clone()
+
+#     git.clone.assert_called_once_with(
+#         repository="git@github.com:steinwurf/giit.git",
+#         directory=git_repository.giit_clone_path,
+#         cwd=git_repository.clone_path)
+
+#     git.fetch.assert_not_called()
+
+#     # Reset call mocks
+
+#     git.clone = mock.Mock()
+
+#     os.makedirs(git_repository.giit_clone_path)
+
+#     # The git repository now already exists so we should see a fetch
+#     git_repository.clone()
+
+#     git.clone.assert_not_called()
+#     git.fetch.assert_called_once_with(
+#         cwd=git_repository.giit_clone_path, all=True, prune=True)
+
+
+# def test_git_checkout(testdirectory, caplog):
+
+#     giit_dir = testdirectory.mkdir('giit')
+#     repo_dir = testdirectory.mkdir('repo')
+
+#     factory = giit.factory.resolve_factory(
+#         giit_path=giit_dir.path(), repository=repo_dir.path())
+
+#     git = fake_git()
+
+#     factory.provide_value(name='git', value=git, override=True)
+
+#     git_repository = factory.build()
+
+#     git_repository.checkout_branch(checkout="improvements-v2")
+#     git.reset.assert_called_once_with(
+#         branch="origin/improvements-v2", hard=True, cwd=git_repository.giit_clone_path)
+
+#     # Reset the mock
+#     git.reset = mock.Mock()
+
+#     git_repository.checkout_branch(checkout="origin/improvements-v2")
+#     git.reset.assert_called_once_with(
+#         branch="origin/improvements-v2", hard=True, cwd=git_repository.giit_clone_path)
+
+def test_git_source_branch_url(testdirectory, caplog):
+    """ Test we have no source branch when the repository is an URL """
+
+    giit_dir = testdirectory.mkdir('giit')
+
+    factory = giit.factory.resolve_factory(
+        giit_path=giit_dir.path(), repository="https://github.com")
+
+    git = fake_git()
+
     factory.provide_value(name='git', value=git, override=True)
 
     git_repository = factory.build()
 
-    git_repository.clone()
-
-    # Add some tests
-    # assert 0
+    assert git_repository.source_branch() == None
 
 
-def test_git_checkout(testdirectory, caplog):
+def test_git_source_branch_no_current(testdirectory, caplog):
+    """ Test that we have no source branch when the current branch is not
+        in the list of remote branches
+    """
 
     giit_dir = testdirectory.mkdir('giit')
     repo_dir = testdirectory.mkdir('repo')
 
-    remotes = ["origin/add-prune",
-               "origin/fix-checkout-in-variables",
-               "origin/improvements-v2",
-               "origin/initial-push",
-               "origin/master",
-               "origin/modify-scopes"]
-
     factory = giit.factory.resolve_factory(
         giit_path=giit_dir.path(), repository=repo_dir.path())
 
-    git = mock.Mock()
-    git.version.remote_branches = remotes
+    git = fake_git()
+    git.current_branch.return_value = "this-branch-does-not-exist"
 
     factory.provide_value(name='git', value=git, override=True)
 
     git_repository = factory.build()
 
-    git_repository.checkout_branch(checkout="master")
+    assert git_repository.source_branch() == None
+
+
+def test_git_source_branch(testdirectory, caplog):
+    """ Test that we get the source branch when the current branch is in the
+        list of remote branches
+    """
+
+    giit_dir = testdirectory.mkdir('giit')
+    repo_dir = testdirectory.mkdir('repo')
+
+    factory = giit.factory.resolve_factory(
+        giit_path=giit_dir.path(), repository=repo_dir.path())
+
+    git = fake_git()
+    git.current_branch.return_value = 'master'
+
+    factory.provide_value(name='git', value=git, override=True)
+
+    git_repository = factory.build()
+
+    assert git_repository.source_branch() == "origin/master"
